ffbbde4a7407d442dcff3ef25dcb39d8ad9d0e3c
MDecker-MobileComputing <mide_42b@yahoo.de>
2025-05-12 18:12:05 +0200

    StringToolsApplicationTests fuer Rechneruebung vorbereitet

--- src/test/java/de/eldecker/dhbw/spring/stringtools/StringToolsApplicationTests.java
+++ src/test/java/de/eldecker/dhbw/spring/stringtools/StringToolsApplicationTests.java
@@ -1,13 +1,28 @@
 package de.eldecker.dhbw.spring.stringtools;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
 
 @SpringBootTest
-class StringToolsApplicationTests {
+@AutoConfigureMockMvc
+class StringToolsRestControllerTest {
 
-	@Test
-	void contextLoads() {
-	}
+	private static final String BASIS_URL = "/stringtools/v1/laenge/";
+
+	@Autowired
+	private MockMvc _mockMvc;
+
+
 
 }
